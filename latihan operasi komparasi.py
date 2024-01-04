27dec6323e5dc65e4f3898f6797af5acb207c251
hadanaM <153629972+hadanaM@users.noreply.github.com>
2024-01-04 21:48:18 +0700

    hadana

---
+++ latihan operasi komparasi.py
@@ -0,0 +1,26 @@
+# operasi komparsi (membandingkan nilai)
+# setiap hasil dari komparasi adalah boolean (true/false)
+# >,<,>=,<=,==,!=,is,is not
+
+a = 4
+b = 2
+# lebih dari
+print("===Lebih Dari(>)===")
+hasil = a > 3
+print(a,'>',3,'=' ,hasil)
+hasil = b > 3
+print(b,'>',3,'=' ,hasil)
+
+# Kurang dari
+print("===Kurang Dari(<)===")
+hasil = a < 3
+print(a,'<',3,'=' ,hasil)
+hasil = b < 4
+print(b,'<',4,'=' ,hasil)
+
+# Lebih dari sama dengan
+print("===Lebih Dari Sama Dengan(>=)===")
+hasil = a >= 3
+print(a,'>=',3,'=' ,hasil)
+hasil = b >= 3
+print(b,'>=',3,'=' ,hasil)
---
+++ latihan tipe data.py
@@ -0,0 +1,31 @@
+# tipe data1 = angka satuan/tidak ada koma (integer)
+data = 16
+print(type(data)) # cara melihat tipe data
+print("data : ", data, " bertipe : ", type(data))
+
+# tipe data2 = angka dengan koma (float)
+data = 1.5
+print(type(data)) # cara melihat tipe data
+print("data : ", data, " bertipe : ", type(data))
+
+# tipe data3 = kumpulan karakter (string)
+data = "Rahma"
+print(type(data)) # cara melihat tipe data
+print("data : ", data, " bertipe : ", type(data))
+
+# tipe data4 = biner true/false (boolean)
+data = True
+print(type(data)) # cara melihat tipe data
+print("data : ", data, " bertipe : ", type(data))
+
+# tipe data4 = biner true/false (boolean)
+data = False
+print(type(data)) # cara melihat tipe data
+print("data : ", data, " bertipe : ", type(data))
+
+## tipe data khusus
+# bilangan complex
+data = complex(5,6)
+print(type(data)) # cara melihat tipe data
+print("data : ", data, " bertipe : ", type(data))
+
